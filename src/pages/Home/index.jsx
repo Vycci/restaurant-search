b1a0739f4e0f3235479cb8bceffbd388739657a4
Victor Alves <victorhugo_sp4@hotmail.com>
2021-08-31 20:06:56 -0300

    adição do carrossel

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,26 +1,50 @@
 import React, { useState } from 'react';
 
 import TextField, { Input } from '@material/react-text-field';
-import { Container , Search, Logo} from './style';
-import logo from '../../assets/logo.svg'
+import MaterialIcon from '@material/react-material-icon';
+import { Search, Logo, SearchColumn, CarouselTitle, Wrapper, Map, Carousel} from './style';
+import logo from '../../assets/logo.svg';
+import restaurante from '../../assets/restaurante-fake.png';
+import {Card} from '../../components';
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+    };
+
     return(
-        <Container>
-        <Search>
-            <Logo src={logo} alt="logo do restaurante"></Logo>
-            <TextField
-                label="Pesquisar"
-                outlined
-            >
-                <Input
-                    value={inputValue}
-                    onChange={(event) => setInputValue(event.target.value)}
-                />
-            </TextField>
-        </Search>
-        </Container>
+        <Wrapper>
+            <SearchColumn>
+                <Search>
+                    <Logo src={logo} alt="logo do restaurante"></Logo>
+                    <TextField
+                        label="Pesquisar Restaurantes"
+                        outlined
+                        trailingIcon={<MaterialIcon role="button" icon="search" />}
+                    >
+                        <Input
+                            value={inputValue}
+                            onChange={(event) => setInputValue(event.target.value)}
+                        />
+                    </TextField>
+                    <CarouselTitle>Na sua Área</CarouselTitle>
+                    <Carousel {...settings}>
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                        <Card photo={restaurante} />
+                    </Carousel>
+                </Search>
+            </SearchColumn>
+            <Map/>
+        </Wrapper>
     );
 };
 
--- src/pages/Home/style.js
+++ src/pages/Home/style.js
@@ -1,6 +1,12 @@
 import styled from 'styled-components';
+import Slider from 'react-slick';
 
-export const Container = styled.aside`
+export const Wrapper = styled.div`
+    display: flex;
+    flex-direction: row;
+`;
+
+export const SearchColumn = styled.aside`
     background-color: ${(props) => props.theme.colors.background};
     width: 360px;
     height: 100vh;
@@ -17,6 +23,26 @@ export const Search = styled.section`
 
 export const Logo = styled.img`
     width: 230px;
-    padding-bottom: 10px;
+    margin-bottom: 10px;
     align-self: center;
 `;
+
+export const Map = styled.div`
+    background-color: red;
+    width: 500px;
+`;
+
+export const Carousel = styled(Slider)`
+    .slick-slide{
+        margin-right: 16px;
+    }
+`;
+
+export const CarouselTitle = styled.h1`
+    font-family: ${(props) => props.theme.fonts.regularFont};
+    color: ${(props) => props.theme.colors.text};
+    font-size: 24px;
+    font-weight: bold;
+    margin-top: 16px;
+`;
+
